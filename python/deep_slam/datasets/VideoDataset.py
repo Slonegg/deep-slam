cf0491316d002cd2fed94cf678d1e0413d5b423f
Natalia Trifonova <koshman_n@mail.ru>
2017-11-22 13:56:31 -0800

    fixed

--- python/deep_slam/datasets/VideoDataset.py
+++ python/deep_slam/datasets/VideoDataset.py
@@ -2,15 +2,11 @@
 import numpy as np
 import cv2
 import os
-import argparse
-from zipfile import ZipFile
-import io
 
 
 class VideoDataset(object):
     def __init__(self, path, color_mode='rgbd', target_size=None):
         """
-
         :param path: dataset folder
         :param color_mode: rgb or rgbd
         :param target_size:
@@ -18,13 +14,7 @@ class VideoDataset(object):
         self.path = path
         self.color_mode = color_mode
         self.target_size = target_size
-        self.compressed = False
-        if os.path.splitext(path)[1] == '.zip':
-            self.compressed = True
-            self.archive = ZipFile(path, 'a')
-            self.filenames = self.archive.namelist()
-        else:
-            self.filenames = os.listdir(path)
+        self.filenames = os.listdir(path)
 
         if color_mode == 'rgbd':
             self.num_channels = 3
@@ -36,80 +26,34 @@ class VideoDataset(object):
         self.rgb_template = "frame-%06d.color.png"
         self.depth_template = "frame-%06d.depth.png"
         self.pose_template = "frame-%06d.pose.txt"
-        if self.compressed:
-            archive_name = os.path.splitext(os.path.basename(path))[0]
-            self.rgb_template = "%s/%s" % (archive_name, self.rgb_template)
-            self.depth_template = "%s/%s" % (archive_name, self.depth_template)
-            self.pose_template = "%s/%s" % (archive_name, self.pose_template)
-            self.path = ""
         if not target_size:
             rgb_file = os.path.join(self.path, self.rgb_template % (0,))
-            rgb_img = self.imread(rgb_file, cv2.IMREAD_COLOR)
+            rgb_img = cv2.imread(rgb_file, cv2.IMREAD_COLOR)
             self.target_size = [rgb_img.shape[0], rgb_img.shape[1]]
 
-    def imread(self, fname, flags):
-        if self.compressed:
-            data = self.archive.read(fname)
-            img = cv2.imdecode(np.frombuffer(data, np.uint8), flags)
-        else:
-            img = cv2.imread(fname, cv2.IMREAD_COLOR)
+    def imread(self, file, flag):
+        img = cv2.imread(file, flag)
+        if img.shape[0] != self.target_size[0] or img.shape[1] != self.target_size[1]:
+            img = cv2.resize(img, (self.target_size[1], self.target_size[0]),
+                             interpolation=cv2.INTER_AREA)
         return img
 
-    def imwrite(self, fname, img):
-        if self.compressed:
-            _, data = cv2.imencode('.png', img)
-            self.archive.writestr(fname, data)
-        else:
-            cv2.imwrite(fname, img)
+    def get_sample(self, offset):
+        rgb_file = os.path.join(self.path, self.rgb_template % (offset % self.num_samples,))
+        pose_file = os.path.join(self.path, self.pose_template % (offset % self.num_samples,))
+        rgb_img = self.imread(rgb_file, cv2.IMREAD_COLOR)
+        pose = np.loadtxt(pose_file)
+        out = {'rgb': rgb_img, 'pose': pose}
 
-    def loadtxt(self, filename):
-        if self.compressed:
-            filename = self.archive.open(filename)
-        return np.loadtxt(filename)
-
-    def savetxt(self, filename, mat):
-        if self.compressed:
-            f = io.BytesIO()
-            np.savetxt(f, mat)
-            self.archive.writestr(filename, f.getvalue())
-        else:
-            np.savetxt(filename, mat)
-
-
-    def get_batch(self, offset, batchsize=1):
-        img_batch = np.zeros([batchsize, self.target_size[0], self.target_size[1], self.num_channels])
-        depth_batch = np.zeros([batchsize, self.target_size[0], self.target_size[1]])
-        pose_batch = np.zeros([batchsize, 4, 4])
-
-        for idx in range(batchsize):
-            rgb_file = os.path.join(self.path, self.rgb_template % ((offset + idx) % self.num_samples,))
-            pose_file = os.path.join(self.path, self.pose_template % ((offset + idx) % self.num_samples,))
-
-            rgb_img = self.imread(rgb_file, cv2.IMREAD_COLOR)
-            if self.color_mode == 'rgbd':
-                depth_file = os.path.join(self.path, self.depth_template % (offset,))
-                depth_img = self.imread(depth_file, cv2.IMREAD_GRAYSCALE)
-
-                if depth_img.shape[0] != self.target_size[0] or depth_img.shape[1] != self.target_size[1]:
-                    depth_img = cv2.resize(depth_img, (self.target_size[1], self.target_size[0]),
-                                           interpolation=cv2.INTER_AREA)
-                depth_batch[idx, :, :] = depth_img
-            pose = self.loadtxt(pose_file)
-
-            if rgb_img.shape[0] != self.target_size[0] or rgb_img.shape[1] != self.target_size[1]:
-                rgb_img = cv2.resize(rgb_img, (self.target_size[1], self.target_size[0]),
-                                       interpolation=cv2.INTER_AREA)
-
-            img_batch[idx, :, :, :] = rgb_img
-            pose_batch[idx, :, :] = pose
         if self.color_mode == 'rgbd':
-            return img_batch, depth_batch, pose_batch
-        else:
-            return img_batch, pose_batch
+            depth_file = os.path.join(self.path, self.depth_template % (offset,))
+            depth_img = self.imread(depth_file, cv2.IMREAD_ANYDEPTH)
+            out['depth'] = depth_img
+        return out
 
-    def generate_batches(self, batchsize=1):
-        for offset in range(0, self.num_samples, batchsize):
-            yield self.get_batch(offset, batchsize)
+    def generator(self):
+        for offset in range(0, self.num_samples):
+            yield self.get_sample(offset)
 
     def add_sample(self, *args):
         if self.color_mode == 'rgbd':
@@ -119,20 +63,9 @@ class VideoDataset(object):
 
         rgb_file = os.path.join(self.path, self.rgb_template % (self.num_samples,))
         pose_file = os.path.join(self.path, self.pose_template % (self.num_samples,))
-        self.imwrite(rgb_file, img)
-        self.savetxt(pose_file, pose)
+        cv2.imwrite(rgb_file, img)
+        np.savetxt(pose_file, pose)
         if self.color_mode == 'rgbd':
             depth_file = os.path.join(self.path, self.depth_template % (self.num_samples,))
-            self.imwrite(depth_file, depth)
+            cv2.imwrite(depth_file, depth)
         self.num_samples += 1
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dataset')
-    args = parser.parse_args()
-    dataset = VideoDataset(args.dataset)
-    imgs, depth, poses = dataset.get_batch(0, 1)
-    dataset.add_sample(imgs[0], depth[0], poses[0])
-    print(imgs.shape, depth.shape, poses.shape)
-    print(poses)
--- python/deep_slam/datasets/office.py
+++ python/deep_slam/datasets/office.py
@@ -1,30 +1,47 @@
 import urllib.request
 from zipfile import ZipFile
 import os
-from pathlib import Path
 from deep_slam.datasets.VideoDataset import VideoDataset
 
-home = str(Path.home())
-data_dir = os.path.join(home, ".keras/datasets")
 
 def report(count, blocksize, filesize):
     if count % 100 == 0:
         print("%d/%d" % (count*blocksize, filesize))
 
-def load_data():
-    url = "http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/office.zip"
-    if os.path.exists(os.path.join(data_dir, "office")):
-        return VideoDataset(os.path.join(data_dir, "office/seq-01.zip"))
-    elif not os.path.isfile(os.path.join(data_dir, "office.zip")):
-        print("File downloading: %s " % os.path.join(data_dir, "office.zip"))
-        file_name, headers = urllib.request.urlretrieve(url, os.path.join(data_dir, "office.zip"), report)
-    else:
-        file_name = os.path.join(data_dir, "office.zip")
 
-    archive = ZipFile(file_name)
-    "Extract seq-01.zip"
-    archive.extractall(data_dir, ['office/seq-01.zip'])
-    return VideoDataset(os.path.join(data_dir, "office/seq-01.zip"))
+def load_data(video_id=1, data_dir=None):
+    if not data_dir:
+        data_dir = os.path.expanduser('~/.keras/datasets')
+    url = "http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/office.zip"
+    subdir = "seq-%02d" % video_id
+    subarchive = "%s.zip" % subdir
+    full_arhive_path = os.path.join(data_dir, "office.zip")
+    sub_archive_path = os.path.join(data_dir, "office", subarchive)
+    sub_dir_path = os.path.join(data_dir, "office", subdir)
+
+    if not os.path.exists(sub_dir_path) and\
+            not os.path.isfile(sub_archive_path) and \
+            not os.path.isfile(full_arhive_path):
+        # No data at all
+        # Download data
+        print("File downloading: %s " % full_arhive_path)
+        full_arhive_path, headers = urllib.request.urlretrieve(url, full_arhive_path, report)
+    if not os.path.exists(sub_dir_path) and\
+            not os.path.isfile(sub_archive_path):
+        # extract subarchive #video_id from full archive
+        with ZipFile(full_arhive_path) as archive:
+            archive.extractall(data_dir, ['office/' + subarchive])
+    if not os.path.exists(sub_dir_path):
+        # extract files from sub archive
+        with ZipFile(sub_archive_path) as archive:
+            archive.extractall(os.path.join(data_dir, 'office'))
+
+    return VideoDataset(sub_dir_path)
 
 if __name__ == "__main__":
-    load_data()
+    data = load_data()
+    for i, f in enumerate(data.generator()):
+        print(i)
+        print(f['rgb'].shape)
+        print(f['depth'].shape)
+        print(f['pose'])
